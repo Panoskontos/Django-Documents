61739d96e94b1baf69188ec01c4b6f027fc217db
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-03 22:51:17 +0300

    added mysql

---
+++ MYSQL/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
