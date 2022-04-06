7853e4d9dacbf1f331f5d347c1658c62e7e2f1ea
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-06 17:18:53 +0300

    no messsage

--- MultipleUsers/models.py
+++ MultipleUsers/models.py
@@ -1,4 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('student', 'student'),
+        ('teacher', 'teacher'),
+        ('secretary', 'secretary'),
+        ('supervisor', 'supervisor'),
+        ('admin', 'admin'),
+    )
+
+    user_type = models.PositiveSmallIntegerField(
+        choices=USER_TYPE_CHOICES, null=True)
+
+
+class First(models.Model):
+    username = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, null=True)
