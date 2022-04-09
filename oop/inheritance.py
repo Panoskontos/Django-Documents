e56ade60c4f32f62968aea3bbc9db74681f6e785
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-10 00:17:30 +0300

    oop

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,33 +1,118 @@
-
 class Mortal:
-    # def __init__(self):
-    #     self.can_die = 'yes it can'
+    def __init__(self, lifespan=80):
+        self.lifespan = lifespan
 
-    def can_this_die(self):
-        print('Can die')
+    def is_breathing(self):
+        print('Yes it can breath')
 
+    def eats(self):
+        print('It is eating')
 
-class Human:
-    def __init__(self, name='unknown', skill='nothing'):
-        self.name = name
-        self.skill = skill
-        self.breathes = True
-
-    def introduce_yourself(self):
-        print(f'my name is {self.name} and i am good at {self.skill}')
-
-    def walk(self):
-        print('is walking')
+    def what_is_this():
+        print('it is a Mortal function')
 
 
-class Developer(Human, Mortal):
-    pass
+class Earthling:
+    def __init__(self, planet='earth'):
+        self.lifespan = 50
+        self.planet = planet
+
+    def lives_in_earth(self):
+        print('Yes it leaves in earth')
 
 
-d1 = Developer()
+class Animal(Mortal, Earthling):
+    def __init__(self, lifespan, species='mammal'):
+        super().__init__(lifespan)
+        self.species = species
+
+    def log(self):
+        print('. . .Random animal sound. . .')
 
 
-# d1.can_this_die()
-# d1.introduce_yourself()
-print(d1.can_die)
-# print(d1.breathes)
+class Rat(Animal):
+    def sound(self):
+        print(' . making rat sounds  ')
+
+
+class Human(Mortal):
+
+    def introduce(self):
+        print(f'My name is {self.name}')
+
+    # make it a private property getter
+    @property
+    def name(self):
+        return self._name
+
+    # setter
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    # deleter
+    @name.deleter
+    def name(self):
+        del self._name
+
+    # static method
+
+    def describe_class():
+        print('This is a test class to learn oop')
+
+    # when you convert to string
+    # If tou don't have this everytme you print it will show onject in memory
+    def __str__(self):
+        print('converting to string')
+        return 'this is a class Human with name ' + self.name
+
+    # Comparing to objects
+    def __eq__(self, other):
+        if self.name == other.name:
+            return True
+        else:
+            return False
+
+
+# h1 = Human('Ricardo')
+# h1.introduce()
+
+# You can add atributes
+# h1.can_fly = True
+
+# Static method, methods are that are not attched to object but to class
+# Human.describe_class()
+
+# h2 = Human()
+# print(h1 == h2)
+
+# h1.is_breathing()
+
+# a1 = Animal(101)
+# a1.is_breathing()
+# a1.log()
+
+
+# print(a1.lifespan)
+# print(h1.lifespan)
+
+
+# Use print(help( class )) to see the class
+# print(help(Animal))
+
+# All methods and magic methods of object
+# print(dir(a1))
+
+# Animal.what_is_this()
+# print(a1.lifespan)
+
+r1 = Rat(10, 'hybrid')
+# r1.eats()
+# r1.lives_in_earth()
+# r1.sound()
+# print(help(Rat))
+# print(dir(r1.__class__.__class__.__itemsize__.__add__))
+
+
+# Why there is no error??
+print(dir(r1.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__.__class__))
