19262e3758a7ad1119f61096dae24686ad619069
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-17 20:51:56 +0300

    added basket

---
+++ Basket/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import *
+# Create your views here.
+
+
+def home(request):
+    product = Product.objects.all()
+
+    #
+    items = MyBasket_Product.objects.all()
+    frequencies = {}
+    total = 0
+    for i in items:
+        frequencies[i.product.name] = 0
+    for i in items:
+        total += i.product.price
+        frequencies[i.product.name] += 1
+
+    Basketitems = items.count()
+    print(frequencies)
+
+    context = {
+        'product': product,
+        'Basketitems': Basketitems,
+        'total': total,
+        'frequencies': frequencies,
+    }
+
+    return render(request, 'home.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout.html', {})
+
+
+def add_product_to_basket(request, product_id):
+    product = Product.objects.get(name=product_id)
+    basket = MyBasket.objects.get(user=request.user)
+    new = MyBasket_Product(product=product, basket=basket)
+    new.save()
+    return redirect('home')
+
+
+def remove_product_from_basket(request, product_id):
+    product = Product.objects.get(name=product_id)
+    basket = MyBasket.objects.get(user=request.user)
+    new = MyBasket_Product.objects.filter(product=product, basket=basket)
+    if new:
+        new = new[0]
+    new.delete()
+    return redirect('home')
